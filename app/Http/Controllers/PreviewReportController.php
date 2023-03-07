233fa20bdee8a42e12da0869669c5d19d2263807
Rifqi-Irfansyah <rifqiirfansyah518@gmail.com>
2023-03-07 15:49:45 +0700

    refactor: delete variabel

--- app/Http/Controllers/PreviewReportController.php
+++ app/Http/Controllers/PreviewReportController.php
@@ -9,12 +9,12 @@ class PreviewReportController extends Controller
 {
     public function index()
     {
-        // Set the collation of the parameter to match the collation of the column
+    // Set the collation of the parameter to match the collation of the column
     $status = 'Terkirim';
 
     // Call the stored procedure with the modified parameter
-    $report = DB::select('CALL get_reports_by_status(?)', [$status]);
+    $report = DB::select('CALL get_reports_by_status(?)', ['Terkirim']);
 
-        return view('pages.previewReport', ['report' => $report]);
+    return view('pages.previewReport', ['report' => $report]);
     }
 }
